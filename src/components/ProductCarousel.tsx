55a0df22b7c6b405b72f39fbc8e4b2b66b53c956
Dhiraj-j <106970046+Dhiraj-j@users.noreply.github.com>
2023-12-26 17:29:17 +0530

    product carousel component

---
+++ src/components/ProductCarousel.tsx
@@ -0,0 +1,91 @@
+import {FlatList, Image, Pressable, StyleSheet, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {COLORS} from '../theme/Colors';
+
+type Props = {
+  images: Array<string>;
+  width: number;
+  height: number;
+};
+const imgString = '../assets/images/placeholder.png';
+const ProductCarousel = ({images, width, height}: Props) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const flatListRef = useRef<FlatList>(null);
+
+  const renderIndicator = (index: number) => {
+    return (
+      <View
+        style={[
+          styles.indicator,
+          index === currentIndex ? styles.highlight : null,
+        ]}
+        key={index}
+      />
+    );
+  };
+
+  const scrollToIndex = (index: number) => {
+    setCurrentIndex(index);
+    flatListRef?.current?.scrollToIndex({index, animated: true});
+  };
+
+  return (
+    <View style={[styles.imageContainer, {width: width, height: height}]}>
+      <FlatList
+        ref={flatListRef}
+        data={images}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={event => {
+          const newIndex = Math.floor(
+            event.nativeEvent.contentOffset.x /
+              event.nativeEvent.layoutMeasurement.width,
+          );
+          setCurrentIndex(newIndex);
+        }}
+        pagingEnabled
+        renderItem={({item}) => (
+          <View style={[styles.imageContainer, {width: width, height: height}]}>
+            <Image style={styles.image} source={require(imgString)} />
+          </View>
+        )}
+      />
+      <View style={styles.indicatorContainer}>
+        {images.map((_, index) => (
+          <Pressable key={index} onPress={() => scrollToIndex(index)}>
+            {renderIndicator(index)}
+          </Pressable>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default ProductCarousel;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    backgroundColor: COLORS.BLACK1,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  indicatorContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+  },
+  indicator: {
+    width: 30,
+    marginLeft: 5,
+    borderRadius: 10,
+    height: 7,
+    backgroundColor: COLORS.BLACK20,
+  },
+  highlight: {
+    backgroundColor: COLORS.SECONDARY,
+  },
+});
--- src/screens/Product.tsx
+++ src/screens/Product.tsx
@@ -1,34 +1,20 @@
-import {FlatList, Image, Pressable, StyleSheet, Text, View} from 'react-native';
-import React, {useRef, useState} from 'react';
+import {BackHandler, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 import {FONTFAMILY, FONTSIZE} from '../theme/FontStyle';
 import {COLORS} from '../theme/Colors';
 import {SCREEN_WIDTH} from '../theme/Screen';
 import Button from '../components/Button';
 import StarRating from '../components/StarRating';
+import ProductCarousel from '../components/ProductCarousel';
+import BackHeader from '../components/BackHeader';
 type Props = {};
 
 const imgString = '../assets/images/placeholder.png';
-const imgArray = [imgString, imgString, imgString, imgString];
+const imgArray = [imgString, imgString, imgString];
 const Product = (props: Props) => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const flatListRef = useRef<FlatList>(null);
-
-  const renderIndicator = (index: number) => {
-    return (
-      <View
-        style={[styles.indicator, index === currentIndex && styles.highlight]}
-        key={index}
-      />
-    );
-  };
-
-  const scrollToIndex = (index: number) => {
-    setCurrentIndex(index);
-    flatListRef?.current?.scrollToIndex({index, animated: true});
-  };
-
   return (
     <View style={styles.container}>
+      <BackHeader />
       <View style={styles.paddingContainer}>
         <Text style={[styles.heading, {fontFamily: FONTFAMILY.manrope300}]}>
           Thin Choise
@@ -41,34 +27,11 @@ const Product = (props: Props) => {
           <Text style={styles.desc}>110 Reviews</Text>
         </View>
       </View>
-      <View style={styles.imageContainer}>
-        <FlatList
-          ref={flatListRef}
-          data={imgArray}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          onMomentumScrollEnd={event => {
-            const newIndex = Math.floor(
-              event.nativeEvent.contentOffset.x /
-                event.nativeEvent.layoutMeasurement.width,
-            );
-            setCurrentIndex(newIndex);
-          }}
-          pagingEnabled
-          renderItem={({item}) => (
-            <View style={styles.imageContainer}>
-              <Image style={styles.image} source={require(imgString)} />
-            </View>
-          )}
-        />
-        <View style={styles.indicatorContainer}>
-          {imgArray.map((item, index) => (
-            <Pressable key={index} onPress={() => scrollToIndex(index)}>
-              {renderIndicator(index)}
-            </Pressable>
-          ))}
-        </View>
-      </View>
+      <ProductCarousel
+        width={SCREEN_WIDTH}
+        height={SCREEN_WIDTH * 0.6}
+        images={imgArray}
+      />
       <View style={styles.paddingContainer}>
         <View style={styles.priceRow}>
           <Text style={styles.price}>$34.70/KG</Text>
@@ -103,32 +66,6 @@ const styles = StyleSheet.create({
     fontSize: FONTSIZE.h2 * 2,
     color: COLORS.GRAY3,
     lineHeight: FONTSIZE.h2 * 2.5,
-  },
-  imageContainer: {
-    width: SCREEN_WIDTH,
-    height: SCREEN_WIDTH * 0.6,
-    backgroundColor: COLORS.BLACK1,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'contain',
-  },
-  indicatorContainer: {
-    flexDirection: 'row',
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-  },
-  indicator: {
-    width: 30,
-    marginLeft: 5,
-    borderRadius: 10,
-    height: 7,
-    backgroundColor: COLORS.BLACK20,
-  },
-  highlight: {
-    backgroundColor: COLORS.SECONDARY,
   },
   priceRow: {
     flexDirection: 'row',
